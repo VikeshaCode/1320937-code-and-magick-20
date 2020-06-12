38b129a0b82c564afce1872ce7a16fcf3c373a64
VikeshaCode <Vikeshadoc@gmail.com>
2020-06-12 13:16:12 +0300

    Add function

--- js/setup.js
+++ js/setup.js
@@ -4,23 +4,24 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES__COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-
-
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+var wizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-debugger;
+
+var getRandomNumber = function (number) {
+  return Math.round(Math.random() * number);
+};
+
 var generateWizard = function () {
   var wizard = {
-    name: WIZARD_NAMES[Math.round(Math.random() * 7)] + ' ' + WIZARD_SURNAMES[Math.round(Math.random() * 7)],
-    coatColor: COAT_COLOR[Math.round(Math.random() * 5)],
-    eyesColor: EYES__COLOR[Math.round(Math.random() * 4)],
+    name: WIZARD_NAMES[getRandomNumber(7)] + ' ' + WIZARD_SURNAMES[getRandomNumber(7)],
+    coatColor: COAT_COLOR[getRandomNumber(5)],
+    eyesColor: EYES__COLOR[getRandomNumber(4)],
   };
   return wizard;
 };
@@ -37,7 +38,7 @@ var wizards = generateWizards();
 
 
 var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+  var wizardElement = wizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
